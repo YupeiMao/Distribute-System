22c3a98c74844a5ab63828c06e140b4d53c826bd
YupeiMao <32018776+YupeiMao@users.noreply.github.com>
2019-03-13 18:00:34 -0500

    Update mp2.py

--- mp2.py
+++ mp2.py
@@ -1,56 +1,39 @@
 import socket
-import threading
-import sys
-import time
 import argparse
-from threading import Lock
+import threading
 
+central = "sp19-cs425-g04-01.cs.illinois.edu"
 
-# hard-coded IP addresses for service
-address = "sp19-cs425-g04-01.cs.illinois.edu"
-
-# hard-coded IP addresses for service
-port = 4444
+server_checked = False
+client_checked = False
 
 # holds all the connection of server
 connections = []
 
-
-# build server for other nodes
-def build_server(port):
-    # set up sock for listening one node
+def buildServer(port):
+    #create server sock for listening
     sockForListen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sockForListen.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    # create server
+    #initialize socket
+
     host = socket.gethostname()
-    sockForListen.bind((host, port))
-    sockForListen.listen(5)
+    sockForListen.bind((host,port))
+    sockForListen.listen()
+    c, a = sockForListen.accept()
 
     while True:
-        c, a = sockForListen.accept()
-        # create thread for this connection listening
-        cThread = threading.Thread(target=handle_node, args = (c,a))
-        cThread.daemon = True
-        cThread.start()
-        connections.append(c)
+        data = c.recv(1024)
+        if not data:
+            # print fail message
+            fail = " has left"
+            print(fail)
+            c.close()
+            break
+        print(data)
 
 
-# handler for receiving messages from one node
-def handle_node(c, a):
-	# receiving messages from this node
-	while True:
-		data = c.recv(2048)
 
-		# failure detector using EOF
-		if not data:
-			# print fail message
-			c.close()
-			break
-
-		handle_transaction(str(data))
-
-# handle one introduce node
 def handle_introduce(line):
 	neighbor = line.split(" ")
 
@@ -76,41 +59,36 @@ def handle_reply(data):
 			handle_transaction(line)
 	return 0
 
-
-# connect to service and handle the response
-def connect_service(port, name):
-	# create socket for service
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	sock.connect((socket.gethostbyname(address), port))
-	host = socket.gethostname()
-	# send CONNECT to service
-	msg = "CONNECT " + name + " " + str(host) + " " + str(port) + "\n"
-	sock.send(msg.encode())
-	# start receiving message from service
-	while True:
+def connectServer(port, name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        host = socket.gethostbyname(central)
+        sock.connect((host, port))
+    except Exception as e:
+        print("fail to connect to central")
+    send_str = "CONNECT" + name + " " + str(socket.gethostname) + " "+str(port) + "\n"
+    sock.send(send_str.encode())
+    while True:
 		data = sock.recv(2048)
 		if(handle_reply(data)):
 			break
-
-
 def main():
-	# parse the command line
-	parser = argparse.ArgumentParser(description = 'Cryptocurrency')
-	parser.add_argument('name', type=str)
-	parser.add_argument('port', type=int)
-	args = parser.parse_args()
-	name = args.name
-	port = args.port
+    #parse the command line
+    parser = argparse.ArgumentParser(description = "Distributed Chat")
+    parser.add_argument('name', type=str)
+    parser.add_argument('port',type=int)
 
-	server = threading.Thread(target=build_server, args=(port, ))
-	# client = threading.Thread(target=connectService, args=(port, ), kwargs={"name": name})
-	client = threading.Thread(target=connect_service, args=(port, name))
+    args = parser.parse_args()
+    name = args.name
+    port = args.port
 
-	# start server and client
-	server.start()
-	client.start()
+    server = threading.Thread(target=buildServer, args=(port,))
+    client = threading.Thread(target=connectServer, args=(port, name))
 
-# entry point for application
+    server.start()
+    client.start()
+
+
 if __name__ == '__main__':
     main()
